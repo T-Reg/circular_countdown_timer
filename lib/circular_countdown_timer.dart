30d9f3d3287da1d0334e5eb34cc8da459944ba6c
MSI\timre <timregan10@gmail.com>
2024-02-17 22:36:55 -0800

    fix?

--- lib/circular_countdown_timer.dart
+++ lib/circular_countdown_timer.dart
@@ -284,52 +284,49 @@ class CircularCountDownTimerState extends State<CircularCountDownTimer>
       width: widget.width,
       height: widget.height,
       child: AnimatedBuilder(
-          animation: _controller!,
-          builder: (context, child) {
-            return Align(
-              child: AspectRatio(
-                aspectRatio: 1.0,
-                child: Stack(
-                  children: <Widget>[
-                    Positioned.fill(
-                      child: CustomPaint(
-                        painter: CustomTimerPainter(
-                            animation: _countDownAnimation ?? _controller,
-                            fillColor: widget.fillColor,
-                            fillGradient: widget.fillGradient,
-                            ringColor: widget.ringColor,
-                            ringGradient: widget.ringGradient,
-                            strokeWidth: widget.strokeWidth,
-                            strokeCap: widget.strokeCap,
-                            isReverse: widget.isReverse,
-                            isReverseAnimation: widget.isReverseAnimation,
-                            backgroundColor: widget.backgroundColor,
-                            backgroundGradient: widget.backgroundGradient),
-                      ),
+        animation: _controller!,
+        builder: (context, child) {
+          return Align(
+            child: AspectRatio(
+              aspectRatio: 1.0,
+              child: Stack(
+                children: <Widget>[
+                  Positioned.fill(
+                    child: CustomPaint(
+                      painter: CustomTimerPainter(
+                        animation: _countDownAnimation ?? _controller,
+                        fillColor: widget.fillColor,
+                        fillGradient: widget.fillGradient,
+                        ringColor: widget.ringColor,
+                        ringGradient: widget.ringGradient,
+                        strokeWidth: widget.strokeWidth,
+                        strokeCap: widget.strokeCap,
+                        isReverse: widget.isReverse,
+                        isReverseAnimation: widget.isReverseAnimation,
+                        backgroundColor: widget.backgroundColor,
+                        backgroundGradient: widget.backgroundGradient),
                     ),
-                    widget.isTimerTextShown
-                        ? Align(
-                            alignment: FractionalOffset.center,
-                            child: Text(
-                              time,
-                              style: widget.textStyle ??
-                                  const TextStyle(
-                                    fontSize: 16.0,
-                                    color: Colors.black,
-                                  ),
-                              textAlign: widget.textAlign,
-                            ),
-                          )
-                        : (widget.centerWidget != null
-                          ? widget.centerWidget
-                          : Container()
-                        
-                        ),
-                  ],
-                ),
+                  ),
+                  widget.isTimerTextShown
+                      ? Align(
+                          alignment: FractionalOffset.center,
+                          child: Text(
+                            time,
+                            style: widget.textStyle ??
+                                const TextStyle(
+                                  fontSize: 16.0,
+                                  color: Colors.black,
+                                ),
+                            textAlign: widget.textAlign,
+                          ),
+                        )
+                      : widget.centerWidget,
+                ],
               ),
-            );
-          }),
+            ),
+          );
+        },
+      ),
     );
   }
 
